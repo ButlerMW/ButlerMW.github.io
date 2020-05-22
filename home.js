c3ba862a06e02134cd9a5d3ff25d694f2f0b0b0a
Michael Butler <michael_w_butler@live.com>
2020-05-21 21:28:20 -0600

    first commit

---
+++ home.js
@@ -0,0 +1,25 @@
+var slideIndex = 0;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.querySelectorAll('.slide');
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";    // this is the problem - why?
+    setTimeout(showSlides, 3000); //Change every three seconds
+}
