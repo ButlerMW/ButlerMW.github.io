cec7a6b794eea5c9826ad4b52f37f0f5bd3e4864
Michael Butler <michael_w_butler@live.com>
2021-01-20 13:33:03 -0700

    created new changes to homepage

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  Michael Butler
+</h1>
+ 
+😁
+
+I little website that discribes a little bit about me
--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-var slideIndex = 0;
-
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.querySelectorAll('.slide');
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";    // this is the problem - why?
-    setTimeout(showSlides, 3000); //Change every three seconds
+function colorChange() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let newColor = `rgb(${r}, ${g}, ${b})`; 
+  document.getElementById("title").style.color = newColor;
 }
