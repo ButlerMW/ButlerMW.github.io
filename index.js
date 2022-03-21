a549323866fc9f713524ba4c3f72ef1da689dbdb
ButlerMW <butler.m.w@outlook.com>
2022-03-21 07:33:12 -0600

    Cleaned up uneeded and unwated items

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ function ColorChange() {
   // let g = Math.floor(Math.random() * 256);
   // let b = Math.floor(Math.random() * 256);
   // let newTitleColor = `rgb(${r}, ${g}, ${b})`; 
+  document.getElementById("main").style.backgroundColor = null;
+
   document.getElementById("title").style.color = RandomColorizer();
 
   // r = Math.floor(Math.random() * 256);
   // g = Math.floor(Math.random() * 256);
   // b = Math.floor(Math.random() * 256);
   // let newButtonColor = `rgb(${r}, ${g}, ${b})`; 
-  var buttons = document.getElementsByClassName("color-change");
-  for(var i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundColor = RandomColorizer();
-    buttons[i].style.color = RandomColorizer();
+  var elements = document.getElementsByClassName("color-change");
+  for(var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = RandomColorizer();
+    elements[i].style.color = RandomColorizer();
   }
 }
 
@@ -23,4 +25,21 @@ function RandomColorizer() {
   let randomColor = `rgb(${r}, ${g}, ${b})`; 
   
   return randomColor;
+}
+
+function DarkMode() {
+  // document.footer.style.color = "#121212";
+  var elements = document.getElementsByClassName("color-change");
+  for(var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = RandomColorizer();
+    elements[i].style.color = RandomColorizer();
+  }
+
+  // document.documentElement.style.setProperty('--ocean-green', '#121212');
+  document.getElementById("container").style.backgroundColor = "#121212";
+  document.getElementById("title").style.color = "#d5f2e3"; 
+  document.getElementById("footer").style.backgroundColor = "#121212"; 
+  // document.body.style.backgroundColor = "#121212";
+  // document.body.style.backgroundImage = null;
+  document.getElementById("main").style.backgroundColor = "#121212";
 }
