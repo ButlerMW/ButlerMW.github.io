399c3d0ed9ee3a46e6832c60baacf2af68a4e782
ButlerMW <michael_w_butler@live.com>
2025-03-21 22:26:07 -0600

    add fading feature to navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Placeholder
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    
+    if (window.scrollY > 0) {
+        navbar.style.opacity = this.window.scrollY / 100;
+    } else {
+        navbar.style.opacity = 0;
+    }
+});
